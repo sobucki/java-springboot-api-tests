a639a64ecc0b0a708a011e09cdc7f472107135bf
Rafael Sobucki <sobucki@Mac-mini-de-Rafael.local>
2025-05-15 17:26:39 -0300

    Create category service

--- src/main/java/br/com/sobucki/productmanager/service/CategoryService.java
+++ src/main/java/br/com/sobucki/productmanager/service/CategoryService.java
@@ -1,11 +1,13 @@
 package br.com.sobucki.productmanager.service;
 
 import java.util.List;
+import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.com.sobucki.productmanager.dto.CategoryDTO;
+import br.com.sobucki.productmanager.model.Category;
 import br.com.sobucki.productmanager.repository.category.CategoryRepository;
 
 @Service
@@ -19,8 +21,51 @@ public class CategoryService {
 
   public List<CategoryDTO> getAllCategories() {
     return repository.findAll().stream()
-        .map(category -> new CategoryDTO(category.getId(), category.getName(), category.getDescription()))
+        .map(category -> createCategoryDTO(category))
         .toList();
+  }
+
+  public CategoryDTO createCategory(CategoryDTO categoryDTO) {
+    Category category = createCategoryFromDTO(categoryDTO);
+    Category savedCategory = repository.save(category);
+    return createCategoryDTO(savedCategory);
+  }
+
+  public CategoryDTO updateCategory(UUID id, Category product) {
+    Category existingCategory = repository.findById(id);
+    if (existingCategory == null) {
+      return null;
+    }
+
+    existingCategory.setName(product.getName());
+    existingCategory.setDescription(product.getDescription());
+
+    repository.save(existingCategory);
+
+    return createCategoryDTO(existingCategory);
+  }
+
+  public void deleteCategory(UUID id) {
+    repository.delete(id);
+  }
+
+  public CategoryDTO getCategoryById(UUID id) {
+    Category category = repository.findById(id);
+    if (category == null) {
+      return null;
+    }
+    return createCategoryDTO(category);
+  }
+
+  public Category createCategoryFromDTO(CategoryDTO categoryDTO) {
+    Category category = new Category();
+    category.setName(categoryDTO.getName());
+    category.setDescription(categoryDTO.getDescription());
+    return category;
+  }
+
+  public CategoryDTO createCategoryDTO(Category category) {
+    return new CategoryDTO(category.getId(), category.getName(), category.getDescription());
   }
 
 }
--- src/test/java/br/com/sobucki/productmanager/service/CategoryServiceTest.java
+++ src/test/java/br/com/sobucki/productmanager/service/CategoryServiceTest.java
@@ -1,35 +1,126 @@
 package br.com.sobucki.productmanager.service;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import static org.mockito.Mockito.*;
+import static org.mockito.ArgumentMatchers.any;
 
 import java.util.List;
+import java.util.UUID;
 
 import br.com.sobucki.productmanager.dto.CategoryDTO;
+import br.com.sobucki.productmanager.model.Category;
 import br.com.sobucki.productmanager.repository.category.CategoryRepository;
 
 public class CategoryServiceTest {
 
   private CategoryRepository repository;
-  private CategoryService productService;
+  private CategoryService categoryService;
 
   @BeforeEach
-  void setUp() {
+  public void setUp() {
     repository = mock(CategoryRepository.class);
-    productService = new CategoryService(repository);
+    categoryService = new CategoryService(repository);
   }
 
   @Test
   @DisplayName("Should return all categories")
-  void shouldReturnAllCategories() {
+  public void shouldReturnAllCategories() {
     when(repository.findAll()).thenReturn(List.of());
-    List<CategoryDTO> categories = productService.getAllCategories();
+    List<CategoryDTO> categories = categoryService.getAllCategories();
 
     assertEquals(0, categories.size());
     verify(repository).findAll();
+  }
+
+  @Test
+  @DisplayName("Should create a category")
+  public void shouldCreateCategory() {
+    CategoryDTO categoryDTO = new CategoryDTO(null, "Vestuário", "Roupa e Acessórios");
+    UUID id = UUID.randomUUID();
+    when(repository.save(any())).thenReturn(new Category(id, "Vestuário", "Roupa e Acessórios"));
+
+    CategoryDTO createdCategory = categoryService.createCategory(categoryDTO);
+
+    assertEquals("Vestuário", createdCategory.getName());
+    assertEquals("Roupa e Acessórios", createdCategory.getDescription());
+    verify(repository).save(any());
+  }
+
+  @Test
+  @DisplayName("Should update a category")
+  public void shouldUpdateCategory() {
+    UUID id = UUID.randomUUID();
+
+    Category foundCategory = new Category(id, "Vestuário", "Roupa e Acessórios");
+    Category updated = new Category(id, "Vestuário", "Moda");
+
+    when(repository.findById(id)).thenReturn(foundCategory);
+    when(repository.save(foundCategory)).thenReturn(updated);
+    CategoryDTO result = categoryService.updateCategory(id, updated);
+
+    assertEquals("Vestuário", result.getName());
+    assertEquals("Moda", result.getDescription());
+    assertEquals(id, result.getId());
+
+    verify(repository).save(foundCategory);
+    ;
+  }
+
+  @Test
+  @DisplayName("Should delete a category")
+  public void shouldDeleteCategory() {
+    UUID id = UUID.randomUUID();
+    when(repository.delete(id)).thenReturn(true);
+
+    categoryService.deleteCategory(id);
+
+    verify(repository).delete(id);
+  }
+
+  @Test
+  @DisplayName("Should return a category by ID")
+  public void shouldReturnCategoryById() {
+    UUID id = UUID.randomUUID();
+    Category foundCategory = new Category(id, "Vestuário", "Roupa e Acessórios");
+
+    when(repository.findById(id)).thenReturn(foundCategory);
+    CategoryDTO result = categoryService.getCategoryById(id);
+
+    assertEquals("Vestuário", result.getName());
+    assertEquals("Roupa e Acessórios", result.getDescription());
+    assertEquals(id, result.getId());
+
+    verify(repository).findById(id);
+  }
+
+  @Test
+  @DisplayName("Should return null when category not found by ID")
+  public void shouldReturnNullWhenCategoryNotFoundById() {
+    UUID id = UUID.randomUUID();
+
+    when(repository.findById(id)).thenReturn(null);
+    CategoryDTO result = categoryService.getCategoryById(id);
+
+    assertEquals(null, result);
+
+    verify(repository).findById(id);
+  }
+
+  @Test
+  @DisplayName("Should return null when updating a non-existing category")
+  public void shouldReturnNullWhenUpdatingNonExistingCategory() {
+    UUID id = UUID.randomUUID();
+    Category updatedCategory = new Category(id, "Vestuário", "Roupa e Acessórios");
+
+    when(repository.findById(id)).thenReturn(null);
+    CategoryDTO result = categoryService.updateCategory(id, updatedCategory);
+
+    assertEquals(null, result);
+
+    verify(repository).findById(id);
   }
 }
